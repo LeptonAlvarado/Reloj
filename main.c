87225023fbc43a8b999e790502c41d4d3ca7322f
Josué Alvarado González <oscarjosue_alvarado@ucol.mx>
2019-06-04 12:03:18 -0500

    Se agregaron las interrupciones

--- main.c
+++ main.c
@@ -13,8 +13,28 @@
 
 #use delay(clock=16000000)
 
+int countTimer=0;
+#int_timer0
+void isr_timer0()
+{
+    countTimer++;
+    set_timer0(15536);
+}
+
+#int_RDA
+void isr_serial()
+{
+}
+
 void main()
 {
+    setup_oscillator(OSC_16MHZ);
+    setup_adc_ports(NO_ANALOGS);
+    setup_timer_0(RTCC_INTERNAL|RTCC_DIV_8);
+    set_timer0(15536);
+    enable_interrupts(INT_TIMER0);
+    enable_interrupts(INT_RDA);
+    enable_interrupts(GLOBAL);
     while(TRUE)
     {
        
